75a321f28dc81a00e6fec2d87b4b8c4a765d0196
Narasimhan C <narasimhan.c.12@gmail.com>
2017-12-06 08:03:30 +0530

    Updated server unit tests

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,30 +1,30 @@
 
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../server');
 let should = chai.should();
 
 chai.use(chaiHttp);
 
 describe('Server Test', () => {
+
+    var url = 'http://localhost:3000';
     beforeEach((done) => {
-         
+         done();
     });
   /*
   * Test the /POST route
   */
   describe('/POST api/space ', () => {
-      it('it should POST with empty body', (done) => {
-                                                                                                                                                
-        chai.request(server)
+      it('it should POST with empty body and get back status 200 with message Request timeout', (done) => {
+        chai.request(url)
             .post('/api/space')
             .send({})
             .end((err, res) => {
+                should.not.exist(err);
                 res.should.have.status(200);
                 res.body.should.be.a('object');
-                res.body.should.have.property('errors');
-                res.body.errors.should.have.property('pages');
-                res.body.errors.pages.should.have.property('kind').eql('required');
+                res.body.should.have.property('message');
+                res.body.message.should.equal('RequestTimedOutError: Request timed out');
               done();
             });
       });
